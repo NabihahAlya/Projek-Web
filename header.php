68456758b5040cc87f2e46df1de0be8299fdeeb9
Nabihah Alya Az Zahidah <nabihahalya24@gmail.com>
2025-04-24 07:36:25 +0700

    add halaman login

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Header</title>
+    <title>Helios Hotel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
@@ -41,10 +41,10 @@
           <a href="wisata.php"><i class="fas fa-plane"></i>Wisata</a>
         </li>
         <li class="navi">
-          <a href="hubungi-kami.php"><i class="fa-solid fa-tty"></i>Kritik & Saran</a>
+          <a href="kritik-saran.php"><i class="fa-solid fa-tty"></i>Kritik & Saran</a>
         </li>
         <li class="navi">
-          <a href="#"><i class="fa-solid fa-user"></i></a>
+          <a href="login.html"><i class="fa-solid fa-user"></i></a>
         </li>
       </ul>
     </nav>
